6547855e84e75c5533480ccc30984833c27256d1
Don Lin <don.lin@optimalworkshop.com>
2024-12-02 09:18:15 +1300

    update tab space from 4 to 2 spaces

--- hilo.rb
+++ hilo.rb
@@ -7,22 +7,22 @@ number = rand(1...101)
 
 # conditional logic that is run as long as user has remaining guesses left
 while guesses > 0
-    # get user input
-    puts "Guess the number between 1 & 100 (inclusive):"
-    # store user input
-    guess = gets.chomp.to_i
-    # if user guesses correctly, game is won (guess = number)
-    if (guess == number)
-        puts "You win! #{number} was the number!"
-        break
-    # if guess too high, decrement number of guesses left and tell them their guess was too high
-    elsif guess > number
-        guesses -= 1
-        puts "Your guess is too high!"
-    # if guess too low, decrement number of guesses left and tell them their guess was too low
-    elsif guess < number
-        guesses -=1
-        puts "Your guess is too Low!"
+  # get user input
+  puts "Guess the number between 1 & 100 (inclusive):"
+  # store user input
+  guess = gets.chomp.to_i
+  # if user guesses correctly, game is won (guess = number)
+  if (guess == number)
+    puts "You win! #{number} was the number!"
+    break
+  # if guess too high, decrement number of guesses left and tell them their guess was too high
+  elsif guess > number
+    guesses -= 1
+    puts "Your guess is too high!"
+  # if guess too low, decrement number of guesses left and tell them their guess was too low
+  elsif guess < number
+    guesses -=1
+    puts "Your guess is too Low!"
     end
 end
 
